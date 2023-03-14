4651b41e306e72a5326d8905d24e353d0741ef4f
sunchao <sunchao-b4@boe.com.cn>
2023-03-14 14:57:01 +0800

    fix: amend

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:sunchao153/ziBrainDocs.git master:gh-pages
+git push -f git@github.com:sunchao153/ziBrainDocs.git main:gh-pages
 
 cd -
